fce181ca2da625687b9cf3583b3f5cffa1ce0a92
Kim Sandberg <s@ndberg.dk>
2020-09-30 12:31:38 +0200

    NOPE no dice

---
+++ rulesencyclopediabackend/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Ajax.Utilities;
+using rulesencyclopedia;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace rulesencyclopediabackend.Controllers
+{
+    public class UserController : ApiController
+    {
+        UserDTO[] user = new UserDTO[10];
+        // GET api/users
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/users/5
+        public UserDTO Get(int id)
+        {
+            if (id < 10)
+            {
+                if(user[id]!=null)
+                {
+                    return user[id];
+                }
+            }
+            return null;
+            
+        }
+
+        // POST api/users
+        public void Post([FromBody] UserDTO user)
+        {
+            if (this.user.Length < 11)
+            {
+                int numberOfUsers = this.user.Length;
+
+                UserDTO newUser = new UserDTO();
+                newUser.ID = this.user.Length;
+                newUser.FirstName = user.FirstName;
+                newUser.MiddleName = user.MiddleName;
+                newUser.LastName = user.LastName;
+                newUser.UserName = user.UserName;
+                newUser.Password = user.Password;
+                newUser.Date = user.Date;
+                this.user[newUser.ID] = newUser;
+            }
+            else
+            {
+             
+            }
+
+        }
+
+        // PUT api/users/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ rulesencyclopediabackend/Models/UserDTO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rulesencyclopedia
+{
+    public class UserDTO
+    {
+        public int ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string FirstName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string MiddleName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string LastName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string UserName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Password
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public DateTime Date
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
