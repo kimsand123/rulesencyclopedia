948208968e5920c2902d3ca13051e89e43c95e30
Kim Sandberg <s@ndberg.dk>
2020-10-01 13:38:08 +0200

    Added the client starting to create DAL for User

---
+++ rulesencyclopediaclient/DAL/UserDAO.cs
@@ -0,0 +1,29 @@
+﻿using rulesencyclopediaclient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rulesencyclopediaclient.DAL
+{
+    class UserDAO
+    {
+        public HttpWebResponse createUser(UserDTO user)
+        {
+
+        }
+
+        public HttpWebResponse loginUser(String userName, String Password)
+        {
+
+        }
+
+        public HttpWebResponse getUser(String token)
+        {
+
+        }
+
+    }
+}
---
+++ rulesencyclopediaclient/Model/UserDTO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rulesencyclopediaclient.Model
+{
+    class UserDTO
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string MiddleName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+
+        }
+
+        public DateTime Date
+        {
+            get;
+            set;
+
+        }
+    }
+}
