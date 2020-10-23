6c8f86a086e21967c3160db51ec4ade003613102
Kim Sandberg <s@ndberg.dk>
2020-10-23 19:22:04 +0200

    Client start login windows visual done

--- DB/DDL/dbo.Game.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Game] (
-    [Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [Name]     VARCHAR (100) NOT NULL,
-    [Company]  VARCHAR (100) NOT NULL,
-    [Revision] INT           NOT NULL,
-    [Editor]   VARCHAR (50)  NULL,
-    CONSTRAINT [PK_Game] PRIMARY KEY CLUSTERED ([Id] ASC),
-);
-
--- rulesencyclopediabackend/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace rulesencyclopediabackend.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
