d9ae7410f479d4d6a32653230cd46e4c1cbff141
Kim Sandberg <s@ndberg.dk>
2020-10-23 20:18:47 +0200

    hmm not much closer

--- rulesencyclopediaclient/DAL/UserDAO.cs
+++ rulesencyclopediaclient/DAL/UserDAO.cs
@@ -1,4 +1,5 @@
 ﻿using rulesencyclopediaclient.Model;
+using rulesencyclopediaclient.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +7,29 @@ using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace rulesencyclopediaclient.DAL
 {
     class UserDAO
     {
-        public HttpWebResponse createUser(UserDTO user)
+        static HttpClient client = new HttpClient();
+        static async Task<HttpResponseMessage> CreateUserAsync(UserDTO user)
         {
-
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+               "api/User", user);
+            response.EnsureSuccessStatusCode();
+            return response;
         }
 
         public async Task loginUser(String userName, String Password)
         {
-            string url = "/api/User/{userName}{Password}";
+            string url = "/api/Login/{userName}{Password}";
+
+
         }
 
         public HttpWebResponse getUser(String token)
--- rulesencyclopediaclient/Model/UserDTO.cs
+++ rulesencyclopediaclient/Model/UserDTO.cs
@@ -1,55 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace rulesencyclopediaclient.Model
+namespace rulesencyclopediaclient.Models
 {
-    class UserDTO
+    public class UserDTO
     {
-        public int ID
-        {
-            get;
-            set;
-        }
-
-        public string FirstName
-        {
-            get;
-            set;
-        }
-
-        public string MiddleName
-        {
-            get;
-            set;
-        }
-
-        public string LastName
-        {
-            get;
-            set;
-        }
-
-        public string UserName
-        {
-            get;
-            set;
-        }
-
-        public string Password
-        {
-            get;
-            set;
-
-        }
-
-        public DateTime Date
-        {
-            get;
-            set;
-
-        }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
     }
 }
